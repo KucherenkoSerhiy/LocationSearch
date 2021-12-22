757bb5c1b13827bc89d13ea608133ca72a1e100e
skucherenko <kucherenkoserhiy@gmail.com>
2021-12-22 14:34:42 +0100

    tested and added RetrieveLocationsQueryHandler

---
+++ src/LocationSearch.Application/Location/Models/Handlers/RetrieveLocationsQueryHandler.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using LocationSearch.Application.Location.Models.Queries;
+using LocationSearch.Application.Location.Models.Queries.Responses;
+using LocationSearch.Application.Location.Models.Services;
+using MediatR;
+
+namespace LocationSearch.Application.Location.Models.Handlers
+{
+    public class RetrieveLocationsQueryHandler : IRequestHandler<RetrieveLocationsQuery, RetrieveLocationsQueryResponse>
+    {
+        private readonly IRetrieveLocationsAppService _retrieveLocationsAppService;
+
+        public RetrieveLocationsQueryHandler(IRetrieveLocationsAppService retrieveLocationsAppService)
+        {
+            _retrieveLocationsAppService = retrieveLocationsAppService;
+        }
+
+        public async Task<RetrieveLocationsQueryResponse> Handle(RetrieveLocationsQuery request,
+            CancellationToken cancellationToken)
+        {
+            var response = new RetrieveLocationsQueryResponse
+            {
+                Locations = await _retrieveLocationsAppService.Retrieve(request.Parameters)
+            };
+            return response;
+        }
+    }
+}
---
+++ test/LocationSearch.Application.Test/Location/Handlers/RetrieveLocationsHandlerTest.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using LocationSearch.Application.Location.Models.Handlers;
+using LocationSearch.Application.Location.Models.Queries;
+using LocationSearch.Application.Location.Models.Queries.Responses;
+using LocationSearch.Application.Location.Models.Services;
+using LocationSearch.Domain.Location.Models;
+using Moq;
+using NUnit.Framework;
+
+namespace LocationSearch.Application.Test.Location.Handlers
+{
+    [TestFixture]
+    [Parallelizable(ParallelScope.All)]
+    public class RetrieveLocationsHandlerTest
+    {
+        [Test]
+        public void Handle_ValidRequest_Ok()
+        {
+            var queryParameters = new LocationQueryParams();
+            var query = new RetrieveLocationsQuery
+            {
+                Parameters = queryParameters
+            };
+            var locations = new List<Domain.Location.Models.Location>();
+            var response = new RetrieveLocationsQueryResponse
+            {
+                Locations = locations
+            };
+
+            var sut = GetSut(out var startLocationAppServiceMock);
+            startLocationAppServiceMock
+                .Setup(x => x.Retrieve(queryParameters))
+                .Returns(() => Task.FromResult(locations));
+
+            var actualResponse = sut.Handle(query, CancellationToken.None).Result;
+
+            Assert.AreSame(response.Locations, actualResponse.Locations);
+            startLocationAppServiceMock.Verify(x => x.Retrieve(queryParameters), Times.Once);
+        }
+
+        private static RetrieveLocationsQueryHandler GetSut(
+            out Mock<IRetrieveLocationsAppService> retrieveLocationsAppService)
+        {
+            retrieveLocationsAppService = new Mock<IRetrieveLocationsAppService>(MockBehavior.Strict);
+            return new RetrieveLocationsQueryHandler(retrieveLocationsAppService.Object);
+        }
+    }
+}
